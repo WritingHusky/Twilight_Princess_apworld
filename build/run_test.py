e8996a0ae6ad97912a2eb968e266e7335d1fa210
Ethan Gelinas <115745794+WritingHusky@users.noreply.github.com>
2025-02-11 16:12:36 -0800

    Add script that runs tests for the world and general ap tests

---
+++ build/run_test.py
@@ -0,0 +1,15 @@
+import pytest  # type: ignore
+
+
+# This will create host.yaml and custom worlds folder as it uses relative paths
+def main():
+    test_directories = ["worlds\\twilight_princess_apworld\\tests", "test\\general"]
+
+    pytest_args = test_directories
+
+    # Run pytest
+    pytest.main(pytest_args)
+
+
+if __name__ == "__main__":
+    main()
