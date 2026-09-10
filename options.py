0931f3a224437d0602e89b79359ba9368d406eb0
Kizugaya <75861024+Kizugaya@users.noreply.github.com>
2026-09-10 03:11:22 +0200

    Rename Bublin to Bulblin

--- options.py
+++ options.py
@@ -454,10 +454,10 @@ class SkipLakebedEntrance(Toggle):
 class SkipArbitersGroundsEntrance(Toggle):
     """
     NON-DEFAULT CHOICE NOT REPRESENTED IN GAME (must self enforce if changed from default)
-    If enabled, entering Arbiters Grounds does not require defeating King Bublin.
+    If enabled, entering Arbiters Grounds does not require defeating King Bulblin.
     """
 
-    display_name = "Arbiters Grounds Does not require Bublin Camp"
+    display_name = "Arbiters Grounds Does not require Bulblin Camp"
     default = True
 
 
